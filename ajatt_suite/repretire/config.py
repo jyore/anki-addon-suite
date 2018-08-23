f06cf4637cf349b53689961ee65787a5461bc251
jyore <joey@jyore.com>
2018-08-23 14:03:56 -0500

    repretire done

--- ajatt_suite/repretire/config.py
+++ ajatt_suite/repretire/config.py
@@ -11,6 +11,8 @@ conf = os.path.join(mw.pm.addonFolder(), 'ajatt_suite/repretire/config.json')
 defaults = {
     "interval": "60",
     "trigger": True,
+    "action": 0,
+    "delete_suspended": False,
 }
 
 
@@ -28,8 +30,12 @@ class RepRetire:
         mw.connect(self.run_action, SIGNAL("triggered()"), self.run)
         submenu.addAction(self.run_action)
 
-        self.load()
+        self.load()        
 
+
+    def restore(self):
+        self.options.update(defaults)
+        self.populate_settings()
 
     def save(self):
         with open(conf,'w') as f:
@@ -46,46 +52,117 @@ class RepRetire:
                 pass
 
 
+    def populate_settings(self):
+        self.interval.setText(self.options["interval"])
+        self.trigger.setChecked(self.options["trigger"])
+        self.action.setCurrentIndex(self.options["action"])
+        self.delete_suspended.setChecked(self.options["delete_suspended"])
+
+        self.warning_text()
+
+
+    def warning_text(self):
+        if self.action.currentIndex() == 2:
+            self.warn.show()
+            self.delete_suspended.show()
+        else:
+            self.warn.hide()
+            self.delete_suspended.hide()
+
+
+    def update_settings(self):
+        self.options["interval"]         = self.interval.text()
+        self.options["trigger"]          = self.trigger.isChecked()
+        self.options["action"]           = self.action.currentIndex()
+        self.options["delete_suspended"] = self.delete_suspended.isChecked()
+
+
+
     def config(self):
 
-        swin = QDialog(mw)
-        vl   = QVBoxLayout()
-        frm  = QGroupBox("Configre")
-        vl.addWidget(frm)
+        self.swin = QDialog(mw)
+        layout = QVBoxLayout()
+        layout.addWidget(self.create_layout())
+        self.swin.setLayout(layout)
 
-        il = QVBoxLayout()
-        il.addWidget(QLabel("Retire card after intervale (in days):"))
-        fl = QHBoxLayout()
-        field = QLineEdit()
-        field.setText(self.options["interval"])
-        fl.addWidget(field)
-        il.addLayout(fl)
+        self.load()
+        self.populate_settings()
 
-        trigger = QCheckBox("Trigger Enabled")
-        trigger.setChecked(self.options["trigger"])
-        fl.addWidget(trigger)
-        il.addLayout(fl)
-
-
-        hl = QHBoxLayout()
-        save = QPushButton("Save")
-        save.connect(save, SIGNAL("clicked()"), swin, SLOT("accept()"))
-        cancel = QPushButton("Cancel")
-        cancel.connect(cancel, SIGNAL("clicked()"), swin, SLOT("reject()"))
-        hl.addWidget(cancel)
-        hl.addWidget(save)
-        vl.addLayout(hl)
-
-        frm.setLayout(il)
-        swin.setLayout(vl)
-        swin.resize(500, 400)
-
-        if swin.exec_():
+        if self.swin.exec_():
             mw.progress.start(immediate=True)
-            self.options["interval"] = field.text()
-            self.options["trigger"] = trigger.isChecked()
+            self.update_settings()
             self.save()
             mw.progress.finish()
+
+
+
+    def create_layout(self):
+        hz_group_box = QGroupBox("Rep Retire Configuration")
+        layout = QGridLayout()
+
+
+        self.interval = QLineEdit()
+        self.trigger = QCheckBox("Enable Trigger")
+
+        self.defaults = QPushButton("Restore Defaults")
+        self.defaults.clicked.connect(self.restore)
+        self.savebtn = QPushButton("Save")
+        self.savebtn.connect(self.savebtn, SIGNAL("clicked()"), self.swin, SLOT("accept()"))
+        self.cancel = QPushButton("Cancel")
+        self.cancel.connect(self.cancel, SIGNAL("clicked()"), self.swin, SLOT("reject()"))
+
+        self.action = QComboBox()
+        self.action.addItems([
+            "Suspend and Tag (recommended)",
+            "Tag Only",
+            "Delete Card"
+        ])
+        self.action.currentIndexChanged.connect(self.warning_text)
+
+        self.warn = QLabel("Warning: Card deletion cannot be undone")
+        self.warn.setStyleSheet('color: red; font-weight: bold;')
+
+        self.delete_suspended = QCheckBox("Delete Suspended")
+
+        # row 0
+        layout.addWidget(QLabel("Retire card after interval (in days):"), 0, 0, 1, 12)
+
+
+        # row 1
+        layout.addWidget(self.interval, 1, 0, 1, 9)
+        layout.addWidget(self.trigger, 1, 9, 1, 3)
+
+
+        # row 2
+        layout.addWidget(QLabel(""), 2, 0, 1, 12)
+
+
+        # row 3
+        layout.addWidget(QLabel("Action to take on retiring card:"), 3, 0, 1, 12)
+
+
+        # row 4
+        layout.addWidget(self.action,4,0,1,12)
+
+
+        # row 5
+        layout.addWidget(self.warn,5,0,1,6)
+        layout.addWidget(self.delete_suspended,5,7,1,6)
+
+
+        # row 6
+        layout.addWidget(QLabel(""), 6, 0, 1, 12)        
+
+
+        # row ?
+        layout.addWidget(self.defaults, 7, 0, 1, 1)
+        layout.addWidget(self.cancel, 7, 8, 1, 2)
+        layout.addWidget(self.savebtn, 7, 10, 1, 2)
+
+
+        hz_group_box.setLayout(layout)
+        return hz_group_box
+
 
 
     def run(self,reviewer=None,answer=None):
@@ -97,21 +174,64 @@ class RepRetire:
                 if card.ivl > int(self.options["interval"]):
                     ids = [card.id]
                     nids = [card.nid]
-                    message = "Retired card"
                 else:
                     ids = []
                     nids = []
                     message = None
             else:
                 # run over all cards, greater than threshold, not suspended already
-                ids_and_nids = mw.col.db.all("select id,nid from cards where queue != -1 and ivl > ?", self.options["interval"])
+                ids_and_nids = mw.col.db.all("select id,nid from cards where queue != -1 and ivl >= ?", self.options["interval"])
                 ids = [i[0] for i in ids_and_nids]
                 nids = [i[1] for i in ids_and_nids]
-                message = "Retired %s cards" % len(ids_and_nids)
 
-            if len(ids) > 0:
-              mw.col.tags.bulkAdd(nids, _("retired"))
-              mw.col.sched.suspendCards(ids)
 
-            if message:
-              tooltip(message)
+            retired_card = False
+            if self.options["action"] == 0:
+                retired_card = self.suspend(nids)
+
+            if self.options["action"] in [0,1]:
+                retired_card = self.tag(nids)
+
+            if self.options["action"] == 2:
+                if self.options["delete_suspended"]:
+                    ids.extend(i[0] for i in mw.col.db.all("select id from cards where queue = -1 and ivl >= ?", self.options["interval"]))
+
+                retired_card = self.delete(ids)
+
+            if retired_card:
+                self.generate_tooltip(len(ids))
+
+
+
+    def suspend(self, ids):
+        if ids is None or len(ids) <= 0:
+            return False
+
+        mw.col.db.execute("update cards set queue = -1 where nid in %s" % ids2str(ids))
+        return True
+
+
+
+    def tag(self, nids):
+        if nids is None or len(nids) <= 0:
+            return False
+       
+        mw.col.tags.bulkAdd(nids, _("retired"))
+        return True
+      
+
+
+    def delete(self,ids):
+        if ids is None or len(ids) <= 0:        
+            return False
+
+        mw.col.remCards(ids)
+        return True
+
+
+
+    def generate_tooltip(self,length):
+        if length == 1:
+            tooltip("Retired Card")
+        elif length > 1:
+            tooltip("Retired %d Cards" % length)
