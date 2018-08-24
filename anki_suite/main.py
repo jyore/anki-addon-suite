453564fb19bda7b5822fa5274e872e98c79b8a6a
jyore <joey@jyore.com>
2018-08-24 16:26:51 -0500

    removed 1 menu layer

--- anki_suite/main.py
+++ anki_suite/main.py
@@ -3,12 +3,14 @@ from aqt.qt import *
 
 
 from .repretire import repretire
-from .reviewmanager import config as revconf
+from .reviewmanager import easeassistant,passfail
 
 mw.form.menuTools.addSeparator()
 menu = mw.form.menuTools.addMenu("Anki Tool Suite")
 mw.form.menuTools.addSeparator()
 
 
-mw.revconf = revconf.ReviewManager(mw,menu)
+mw.easeassistant = easeassistant.EaseAssistant(mw,menu)
+mw.passfail = passfail.PassFail(mw,menu)
 mw.repretire = repretire.RepRetire(mw,menu)
+
